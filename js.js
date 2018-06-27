56f40bdcafe75ff4bd962ceed72b4be8315d554a
Stephen Vassallo <workstation293@gmail.com>
2018-06-27 13:43:51 +0200

    Update 01
    Play/Choice Animation. Needs Logic/polish

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
 //HighScore screen overlay effect.
 function openNav() {document.getElementById("myNav").style.width = "100%";}
 function closeNav() {document.getElementById("myNav").style.width = "0%";}
+//Choice screen ovrlay effect.
+function openChoice() {document.getElementById("choiceCss").style.right = "0vw";}
+function closeChoice() {document.getElementById("choiceCss").style.right = "-100vw";}
