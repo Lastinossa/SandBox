6fe907f89450c7e87d739b7bbc424347f7e17700
Stephen Vassallo <workstation293@gmail.com>
2018-06-27 09:44:29 +0200

    Basic
    Basic Layout
    No Score Function
    Animated Slider - Not linked to game/
    Play button - Not linked
    HighScore - Working, needs polish.

---
+++ js.js
@@ -0,0 +1,3 @@
+//HighScore screen overlay effect.
+function openNav() {document.getElementById("myNav").style.width = "100%";}
+function closeNav() {document.getElementById("myNav").style.width = "0%";}
